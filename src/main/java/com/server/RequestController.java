f626caa1f16e592c8d9bdf2c0df992aa4da3b7ca
Alan Roddick <aroddick16@gmail.com>
2019-04-22 15:54:20 -0700

    Added dummy json files to work with

--- src/main/java/com/server/RequestController.java
+++ src/main/java/com/server/RequestController.java
@@ -1,32 +1,34 @@
 package com.server;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 @RestController
 public class RequestController {
 
-
-//    @RequestMapping(method= RequestMethod.POST, value="/get")
-//    public Map<String,Object> getQuarter(@RequestBody Map<String, Object> quarter) {
-//        Map<String, Object> obj = new HashMap<String, Object>() {{
-//            put("lastUpdated", "test");
-//        }};
-//        System.out.println(quarter.toString());
-//        System.out.println(obj.toString());
-////        Object num2 = obj.get("lastUpdated");
-////        Object num1 = quarter.get("lastUpdated");
-////        if (num2.equals(num1)) {
-////            return new JSONObject();
-////        }
-//        return obj;
-//    }
-
+    private static final String DATA_DIR = System.getProperty("user.dir") + "/data/";
     @RequestMapping(method= RequestMethod.POST, value="/get")
     public JsonObject getQuarter(@RequestBody JsonObject quarter) {
         JsonObject obj = new JsonObject();
+        JsonReader read;
+        try {
+            JsonReader reader = new JsonReader(new FileReader(DATA_DIR + "general.json"));
+            Gson gson = new Gson();
+            reader.
+            gson.fromJson(reader, obj);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        Gson gson = new Gson();
+        read = reader;
+        obj = reader;
         obj.addProperty("lastUpdated", 100);
         System.out.println(quarter.toString());
         System.out.println(obj.toString());
