276a21f6753bef3736e726533f85361ed13abdbd
Alan Roddick <aroddick16@gmail.com>
2019-04-22 14:49:44 -0700

    initial commit in new branch with server skeleton

---
+++ src/main/java/com/server/RequestController.java
@@ -0,0 +1,42 @@
+package com.wewritecode.server;
+
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class RequestController {
+
+
+//    @RequestMapping(method= RequestMethod.POST, value="/get")
+//    public Map<String,Object> getQuarter(@RequestBody Map<String, Object> quarter) {
+//        Map<String, Object> obj = new HashMap<String, Object>() {{
+//            put("lastUpdated", "test");
+//        }};
+//        System.out.println(quarter.toString());
+//        System.out.println(obj.toString());
+////        Object num2 = obj.get("lastUpdated");
+////        Object num1 = quarter.get("lastUpdated");
+////        if (num2.equals(num1)) {
+////            return new JSONObject();
+////        }
+//        return obj;
+//    }
+
+    @RequestMapping(method= RequestMethod.POST, value="/get")
+    public JsonObject getQuarter(@RequestBody JsonObject quarter) {
+        JsonObject obj = new JsonObject();
+        obj.addProperty("lastUpdated", 100);
+        System.out.println(quarter.toString());
+        System.out.println(obj.toString());
+        if (obj.get("lastUpdated").getAsInt() < quarter.get("lastUpdated").getAsInt()) {
+            return obj;
+        }
+        return new JsonObject();
+    }
+}
