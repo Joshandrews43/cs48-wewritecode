e80487785762636886efaa626fa5a45bb2f3f6b0
Alan Roddick <aroddick16@gmail.com>
2019-04-22 18:09:16 -0700

    Updated method to compare test file with post request and return updated json

--- src/main/java/com/server/RequestController.java
+++ src/main/java/com/server/RequestController.java
@@ -1,7 +1,6 @@
 package com.server;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,31 +8,42 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 @RestController
 public class RequestController {
 
     private static final String DATA_DIR = System.getProperty("user.dir") + "/data/";
+
+
+
     @RequestMapping(method= RequestMethod.POST, value="/get")
     public JsonObject getQuarter(@RequestBody JsonObject quarter) {
         JsonObject obj = new JsonObject();
-        JsonReader read;
         try {
             JsonReader reader = new JsonReader(new FileReader(DATA_DIR + "general.json"));
-            Gson gson = new Gson();
-            reader.
-            gson.fromJson(reader, obj);
+            JsonParser parser = new JsonParser();
+            Object object = parser.parse(reader);
+            obj = (JsonObject) object;
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
-        Gson gson = new Gson();
-        read = reader;
-        obj = reader;
-        obj.addProperty("lastUpdated", 100);
         System.out.println(quarter.toString());
         System.out.println(obj.toString());
-        if (obj.get("lastUpdated").getAsInt() < quarter.get("lastUpdated").getAsInt()) {
-            return obj;
+        System.out.println(obj.get("quarters"));
+        JsonArray test = (JsonArray) obj.get("quarters");
+        JsonObject spring = (JsonObject) test.get(0);
+        System.out.println(spring.get("lastUpdated"));
+        if (spring.get("lastUpdated").getAsInt() > quarter.get("lastUpdated").getAsInt()) {
+            try {
+                JsonReader reader = new JsonReader(new FileReader(DATA_DIR + "Spring2019.json"));
+                JsonParser parser = new JsonParser();
+                Object object = parser.parse(reader);
+                obj = (JsonObject) object;
+                return obj;
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+            }
         }
         return new JsonObject();
     }
