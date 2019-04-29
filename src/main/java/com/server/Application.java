8038ab92756ece0b3110ef2f4fe4812e996b63ad
Grant Clark <grantclark1999@gmail.com>
2019-04-29 15:04:31 -0700

    Removed CommandLineRunner from Application.java

--- src/main/java/com/server/Application.java
+++ src/main/java/com/server/Application.java
@@ -34,20 +34,5 @@ public class Application {
             LOGGER.error(e.getMessage());
         }
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
 }
 
