39e4c7d412e0ea2fc872427f26a9b1f0401e4786
Alan Roddick <aroddick16@gmail.com>
2019-04-30 14:32:47 -0700

    Set up GoldPan utility

---
+++ src/main/java/com/wewritecode/scheduler/GoldPan.java
@@ -0,0 +1,11 @@
+package com.wewritecode.scheduler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoldPan {
+
+    private Schedule baseSchedule;
+    private List<Course> remainingCourses;
+    private List<Schedule> finalSchedules;
+}
---
+++ src/main/java/com/wewritecode/scheduler/Schedule.java
@@ -0,0 +1,10 @@
+package com.wewritecode.scheduler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Schedule {
+
+    private List<Course> courses;
+    private int fitness;
+}
