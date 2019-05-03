e30461957101197095778297b32e42d53bccc03f
Grant Clark <grantclark1999@gmail.com>
2019-05-02 22:59:23 -0700

    Continued work on class structure

--- src/main/java/com/wewritecode/scheduler/GoldPan.java
+++ src/main/java/com/wewritecode/scheduler/GoldPan.java
@@ -1,5 +1,10 @@
+/**
+ * @author Grant Clark
+ */
+
 package com.wewritecode.scheduler;
 
+import com.wewritecode.scheduler.schedule.Course;
 import com.wewritecode.scheduler.schedule.Schedule;
 import org.json.JSONObject;
 
@@ -7,22 +12,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class GoldPan {
+public class Scheduler {
 
-    private JSONObject post;
+    private Schedule baseSchedule;
+    private List<Course> remainingCourses;
+    private Map<String, String> filterOptions;
 
-    private List<Schedule> finalSchedules;
-    private Map<String, String> filters;
-
-    public GoldPan() {
-        finalSchedules = new ArrayList<>();
-    }
-
-    public JSONObject run() {
-        JSONObject response = new JSONObject();
-        // run brute force non-conflict algorithm (ScheduleGen)
-        // run sort schedules by filters (ScheduleFilter)
-        // combine all filtered schedules into a JSONObject
-        return response;
+    /**
+     * Generates a list of non-conflicting schedules.
+     *
+     * @param post JSONObject containing the courses selected.
+     * @return list of schedules that do not conflict.
+     */
+    public static List<Schedule> generate(JSONObject post) {
+        List<Schedule> schedules = new ArrayList<>();
+        // TODO: Implement logic here
+        return schedules;
     }
 }
