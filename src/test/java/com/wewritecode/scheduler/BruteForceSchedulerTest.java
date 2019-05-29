ce13611daea551ea87db1317b59974e85d9143ec
Alan Roddick <aroddick16@gmail.com>
2019-05-28 20:22:15 -0700

    Fixed tests

--- src/test/java/com/wewritecode/scheduler/BruteForceSchedulerTest.java
+++ src/test/java/com/wewritecode/scheduler/BruteForceSchedulerTest.java
@@ -64,7 +64,7 @@ public class BruteForceSchedulerTest {
         ScheduleResponse response = scheduler.generate(request);
 
         List<Schedule> schedules = response.getSchedules();
-        assertEquals(5104, schedules.size());
+        assertEquals(1094, schedules.size());
 
         for (Schedule schedule : schedules) {
             assertEquals(6, schedule.getCourses().size());
