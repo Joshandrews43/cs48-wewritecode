e07c262e7eaee8703b8783cf24568ebd553bdb9d
Grant Clark <grantclark1999@gmail.com>
2019-05-31 11:13:42 -0700

    Changed Gaps Options

--- src/main/java/com/wewritecode/server/controller/MinerController.java
+++ src/main/java/com/wewritecode/server/controller/MinerController.java
@@ -4,5 +4,32 @@
 
 package com.wewritecode.server.controller;
 
+import com.wewritecode.miner.GoldMineCLI;
+import com.wewritecode.miner.GoldMiner;
+import com.wewritecode.server.request.ScrapeRequest;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
 public class MinerController {
+
+    @PutMapping(path = "/api/v1/scrape", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void scrape(@RequestBody ScrapeRequest request) {
+        try {
+            GoldMineCLI.run(request.getQuarter());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PutMapping(path = "/api/v1/scrape/demo")
+    public void scrapeDemo(@RequestBody ScrapeRequest request) {
+        GoldMiner miner = new GoldMiner();
+        miner.getAllData(request.getQuarter());
+    }
+
 }
--- src/main/java/com/wewritecode/server/request/ScrapeRequest.java
+++ src/main/java/com/wewritecode/server/request/ScrapeRequest.java
@@ -5,4 +5,8 @@
 package com.wewritecode.server.request;
 
 public class ScrapeRequest {
+    private String quarter;
+
+    public String getQuarter() { return quarter; }
+    public void setQuarter(String quarter) { this.quarter = quarter; }
 }
